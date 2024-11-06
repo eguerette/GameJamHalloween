5acc24ad828b27fc8da7083d90f82f1fab3d37ab
eguerette <emilycloutierg@gmail.com>
2024-11-06 10:10:22 -0500

    compteur dans script

--- Assets/Scripts/Cauldron.cs
+++ Assets/Scripts/Cauldron.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class Cauldron : MonoBehaviour
 {
+    public float totalObject = 0;
+    private void Start()
+    {
+        totalObject = 0;
+    }
     private void OnCollisionEnter(Collision other)
     {
         
         Destroy(other.gameObject);
+        totalObject ++;
     }
 }
